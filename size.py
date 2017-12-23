c121e1bf5ff23a53545757642139c71cdfc561c5
bootlooper <34727398+bootlooper@users.noreply.github.com>
2017-12-23 10:19:08 -0500

    Eventually canvas size picker

---
+++ size.py
@@ -0,0 +1,12 @@
+#random number
+import random
+#image library pillow
+from PIL import Image, ImageDraw
+
+print(random.randint(0,9))
+if int{1, 3, 5}:
+    im = Image.open(iphonebase.jpg)
+elseif int{2, 8, 9}:
+    im = Image.open(1920x1200.jpg)
+else
+    im = Image.open(1200x900.jpg)
