1a57473b6a92c23157aa825659f4f396b2c27c3f
bootlooper <34727398+bootlooper@users.noreply.github.com>
2018-01-01 10:46:01 -0500

    added hex, started towards getting a random gpio

--- size.py
+++ size.py
@@ -5,12 +5,16 @@ import random
 #image library pillow
 from PIL import Image, ImageDraw
 
-random.randint(0,15)
-if int{1, 3, 5, 13, 15}:
-    im = Image.open(phonebase.jpg)
-elseif int{2, 8, 9, 12}:
-    im = Image.open(1920x1200.jpg)
-elseif int{4, 10, 11, 14}:
-    im = Image.open(1200x900.jpg)
-else
-    end
+numbo = open('num.log', 'w')
+
+hex(random.randint(0,15))
+
+
+#if int{1, 3, 5, 13, 15}:
+#    im = Image.open(phonebase.jpg)
+#elseif int{2, 8, 9, 12}:
+#    im = Image.open(1920x1200.jpg)
+#elseif int{4, 10, 11, 14}:
+#    im = Image.open(1200x900.jpg)
+#else
+#    end
