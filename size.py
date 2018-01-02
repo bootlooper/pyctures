78b970dc633414d5cdfb8fea6814df5661690fc6
bootlooper <34727398+bootlooper@users.noreply.github.com>
2018-01-02 17:49:44 -0500

    Write to num.log

--- size.py
+++ size.py
@@ -8,14 +8,19 @@ from PIL import Image, ImageDraw
 numbo = open('num.log', 'w')
 
 #hex 16 number
-hex(random.randint(0,15))
+rando = hex(random.randint(0,15))
+numbo.write(rando)
 
+
+
+#close num.log, EOF
+numbo.close()
 
 #if int{1, 3, 5, 13, 15}:
 #    im = Image.open(phonebase.jpg)
-#elseif int{2, 8, 9, 12}:
+#elif int{2, 8, 9, 12}:
 #    im = Image.open(1920x1200.jpg)
-#elseif int{4, 10, 11, 14}:
+#elif int{4, 10, 11, 14}:
 #    im = Image.open(1200x900.jpg)
 #else
 #    end
