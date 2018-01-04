9322d3b9236fe403184f25a4f77cee23281cbf00
bootlooper <34727398+bootlooper@users.noreply.github.com>
2018-01-04 11:33:41 -0500

    filled out gpioing

--- gpio.py
+++ gpio.py
@@ -11,7 +11,7 @@ TouchPin = 22
 
 #to get rand for display
 
-#should start nano?
+#should start num?
 
 num = open('num.log')
 
@@ -21,3 +21,31 @@ def start():
 	GPIO.setup(RCLK, GPIO.OUT, initial=GPIO.LOW)
 	GPIO.setup(SRCLK, GPIO.OUT, initial=GPIO.LOW)
 	print 'release jorts'
+	GPIO.setup(TouchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	
+# the 74HC595 chip stuff
+def chip(dat):
+    time.sleep(0.001)
+    GPIO.output(RCLK, GPIO.LOW)
+
+def beep():
+	while True:
+         if not GPIO.input(TouchPin):
+             hc595_shift(num.read())
+             time.sleep(2.0)
+     hc595_shift(num.read())
+     time.sleep(0.060)
+
+#execute on death
+def destroy():
+	GPIO.cleanup()
+	num.close()
+
+#oh god
+if __name__ == '__main__':
+	start()
+	setup()
+	try:
+	     beep()
+	except KeyboardInterrupt:
+	     destroy()
