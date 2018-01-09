a61d74756bcd68dfec1d8b3806ead9975822608e
bootlooper <34727398+bootlooper@users.noreply.github.com>
2018-01-09 10:34:27 -0500

    tried sunfounder's methods, added bit to write to num maybe?

--- gpio.py
+++ gpio.py
@@ -18,7 +18,7 @@ TouchPin = 22
 
 lights = [0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x80]
 
-num = open('num.log','w')
+num = open('num','w+')
 rando = random.choice(lights)
 
 #def numbo():
@@ -26,14 +26,18 @@ rando = random.choice(lights)
         
 #        num.write()
 
+#trying a new method via interwebs; Number GPIOs by its physical location
+
 def start():
     print('starting the generation')
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SDI, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(RCLK, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(SRCLK, GPIO.OUT, initial=GPIO.LOW)
-    print('release jorts')
-    GPIO.setup(TouchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(SDI, GPIO.OUT)
+	GPIO.setup(RCLK, GPIO.OUT)
+	GPIO.setup(SRCLK, GPIO.OUT)
+	GPIO.output(SDI, GPIO.LOW)
+	GPIO.output(RCLK, GPIO.LOW)
+	GPIO.output(SRCLK, GPIO.LOW)
+	print('release jorts')
 
 # the 74HC595 chip stuff
 def chip(dat):
@@ -57,6 +61,7 @@ def beep():
           for q in range(0, len(lights)):
                 chip(lights[q])
                 time.sleep(0.5)
+				num.write(lights[q])
 
 #execute on death
 def destroy():
